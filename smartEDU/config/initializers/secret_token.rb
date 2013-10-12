5b56394db7fade9c61fd63b3d884c322b281b121
abishekk <abishekk@cs.ucla.edu>
2013-10-12 12:09:02 -0700

    New empty rails app (with default db)

---
+++ smartEDU/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmartEDU::Application.config.secret_key_base = '1870e65f14fc69b8a9f0c30e7753a909880e90451a831b655fee023308c5007804392d0fedc40a16526acc494286cb86ab75d020d280c8d920765737e4e12d2e'
