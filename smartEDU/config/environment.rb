178d2c5ab0ddc8bc952490a3c5cb510929b9b694
Rajsekar Manokaran <rajsekar@gmail.com>
2013-10-15 01:29:54 +0530

    added the omniauth framework and setup google based login using oauth2   gem.

--- smartEDU/config/environment.rb
+++ smartEDU/config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+ENV['GOOGLE_KEY'] = '582619618234.apps.googleusercontent.com'
+ENV['GOOGLE_SECRET'] = 'EUzJIOZjPHm8KeVt2GvouM2G'
+
+
 # Initialize the Rails application.
 SmartEDU::Application.initialize!
+
