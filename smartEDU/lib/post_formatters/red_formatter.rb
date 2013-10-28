7239560fc848d9169d91db17614e36d1a2f28c5b
abi <abishekk@gmail.com>
2013-10-28 10:44:56 -0700

    temporary commit: moving from home to work

--- smartEDU/lib/post_formatters/red_formatter.rb
+++ smartEDU/lib/post_formatters/red_formatter.rb
@@ -2,12 +2,16 @@ module Forem
   module Formatters
     class RedPlus < Redcarpet::Render::HTML
       def initialize(config = {
-                       :no_links => true,
+                       # :no_links => true,
+                       :filter_html => true,
                        :hard_wrap => true
                      })
         super
         @markdown = Redcarpet::Markdown.new(self,
-                                           {:fenced_code_blocks => true})
+                                           {
+                                              :autolink => true,
+                                              :fenced_code_blocks => true
+                                            })
       end
 
       def format(text)
@@ -18,6 +22,23 @@ module Forem
       def block_code(code, language)
         Pygments.highlight(code, lexer: language)
       end
+
+      def autolink(link, link_type)
+        puts link, link_type
+        puts "abi"
+        return "autolink function"
+      end
+
+      def link(link, title, content)
+        puts "abi link"
+        return "link function"
+      end
+      # process text to identify youtube links and generate
+      # a div with an iframe embed of the youtube URL
+      def youtube_processor(text)
+      end
+
+
     end
 
   end 
