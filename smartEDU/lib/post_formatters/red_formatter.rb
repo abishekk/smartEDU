02df5c6b01ce9db299059db17dbbe20e31c04fc9
abi <abishekk@gmail.com>
2013-10-29 01:29:12 -0700

    changes to Gemfile to remove the auto_html gem. Youtube url embed works on RedPlus

--- smartEDU/lib/post_formatters/red_formatter.rb
+++ smartEDU/lib/post_formatters/red_formatter.rb
@@ -1,14 +1,15 @@
 module Forem
   module Formatters
     class RedPlus < Redcarpet::Render::HTML
+      include ActionView::Helpers::UrlHelper
       def initialize(config = {
-                       # :no_links => true,
                        :filter_html => true,
                        :hard_wrap => true
                      })
         super
         @markdown = Redcarpet::Markdown.new(self,
                                            {
+                                              :no_intra_emphasis => true,
                                               :autolink => true,
                                               :fenced_code_blocks => true
                                             })
@@ -27,23 +28,38 @@ module Forem
       end
 
       def autolink(link, link_type)
-        puts link, link_type
-        puts "abi"
-        return "autolink function"
+        if link_type == "email" then
+          x = mail_to link, link, :class => "autolink"
+        else
+          x = link_to link, link, :class => "autolink"
+          x += youtube_processor(link)
+        end
+        return x.html_safe
       end
 
       def link(link, title, content)
-        puts "abi link"
-        return "link function"
+        x = link_to content, link, :title => title, :class => "mdlink"
+        x += youtube_processor(link)
+        return x.html_safe
       end
       # process text to identify youtube links and generate
       # a div with an iframe embed of the youtube URL
-      def youtube_processor(text)
+      def youtube_processor(link)
+        youtube_url = link
+        youtube_id = nil
+        if youtube_url[/youtu\.be\/([^\?]*)/]
+          youtube_id = $1
+        else
+          youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+          youtube_id = $5
+        end
+        if (youtube_id.nil?)
+          return nil
+        else
+          data = %Q'<div class="flex-video"><iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe></div>'
+          return data.html_safe
+        end
       end
-
-
     end
-
-  end 
+  end
 end
-
